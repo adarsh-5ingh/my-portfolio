8d4678b41aabaed59ba3ef4b1b8dd0c6c39653b3
adarsh-5ingh <adarsh@glashomes.com>
2025-08-16 20:54:00 +0530

    portfolio-setup

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Adarsh Singh",
+  description: "A Web Developer based in Pune City. I’m very passionate about the work that I do.",
+  keywords: "adarsh, adarshsingh, pune, frontend, web development, JavaScript, Next.js, Pune, software engineer, cosmosiq, myflame, matriye",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,218 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "The Blueprint of My Code",
+      description: "Implementing Best Practices for Clean, Efficient Solutions",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Cosmos IQ",
+      des: "I developed cutting-edge ERP and LMS solutions for universities, educators, and students. These platforms blend advanced technology with user-friendly design to streamline online teaching, learning, and administration.",
+      img: "/frame1.svg",
+      iconLists: ["/re.svg", "/bootstrap.svg", "/javascript.svg", "/redux.svg", "/node-js.svg"],
+      link: "https://cosmosiq.ai",
+    },
+    {
+      id: 2,
+      title: "Dashboards",
+      des: "I created four tailored dashboards for Students, Faculty, ERP, and Admin, enhancing user experience with role-specific functionalities and insights for improved efficiency.",
+      img: "/frame2.svg",
+      iconLists: ["/re.svg","/bootstrap.svg", "/javascript.svg", "/redux.svg", "/tail.svg", "/node-js.svg"],
+      link: "https://user.cosmosiq.ai",
+    },
+    {
+      id: 3,
+      title: "My Flame",
+      des: "With My Flame, effortlessly connect and spark chemistry—your perfect match is just a swipe away!",
+      img: "/frame4.svg",
+      iconLists: ["/html5.svg", "/css3.svg", "/tail.svg", "/javascript.svg"],
+      link: "/",
+    },
+    {
+      id: 4,
+      title: "Job Portal",
+      des: "The Job Portal connects job seekers with employers for easy job searches and application management.",
+      img: "/frame3.svg",
+      iconLists: ["/re.svg","/bootstrap.svg", "/javascript.svg", "/redux.svg", "/tail.svg", "/node-js.svg"],
+      link: "/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "I had a great experience working with Adarsh Singh on my website. From start to finish, he understood my vision and executed it with precision and creativity. I highly recommend Adarsh for his skill and professionalism.",
+      name: "Raghvan Koli",
+      title: "Author",
+    },
+    {
+      quote:
+        "Adarsh is a hardworking and talented web designer. Their dedication and creativity consistently deliver high-quality results. A true asset to any team.",
+      name: "Sunil Garad",
+      title: "Senior Project Head",
+    }
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Software Engineer",
+      company: "Glas",
+      time: "Dec 2024 - Present",
+      desc: "Next JS, TypeScript, Rect JS, GraphQL, Redux, AWS",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+
+    {
+      id: 2,
+      title: "React Developer",
+      company: "Matriye Software",
+      time: "Oct 2023 - Sep 2024",
+      desc: "React JS, Redux, React Query, Jira, Zoho Sprint, Figma, Node JS, Express JS, Tailwind, Bootstrap, MUI, Reactstrap, HTML, CSS, Javascript",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 3,
+      title: "Frontend Web Developer",
+      company: "The Development Studio",
+      time: "Jan 2023 - Jun 2023",
+      desc: "React JS, Next JS, Jira, Figma, Tailwind, Bootstrap, MUI, HTML, CSS, Javascript",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 4,
+      title: "UI UX Designer",
+      company: "GenioBITS Technologies",
+      time: "Feb 2022 - Apr 2022",
+      desc: "User Interface, User Experience, WireFrame, figma, Adobe XD",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    //   id: 4,
+    //   title: "Lead Frontend Developer",
+    //   company: "Matriye Software",
+    //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    //   className: "md:col-span-2",
+    //   thumbnail: "/exp4.svg",
+    // },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      link: "https://github.com/adarsh-5ingh",
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      link: "https://x.com/AdarshSingh6026",
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      link: "https://www.linkedin.com/in/adarshsingh6026/",
+      img: "/link.svg",
+    },
+  ];
