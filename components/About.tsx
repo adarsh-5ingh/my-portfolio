3ef219e0b84027caa48bd33469ffc98230be65d5
adarsh-5ingh <adarsh@glashomes.com>
2025-08-18 03:50:23 +0530

    build error solved

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="w-full flex justify-center items-center min-h-[20rem] flex-col">
       <p className="text-neutral-400 dark:text-neutral-300 text-lg md:text-2xl max-w-4xl mx-auto text-left leading-relaxed mb-10">
-        Namaste 🙏 — I'm a software developer based in 
+        Namaste 🙏 — I&apos;m a software developer based in 
         <LinkPreview url="https://en.wikipedia.org/wiki/Bhopal" imageSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSayT0l89M2PnyfUB9GFaF2Mu0LUS6GBqdYA&s" isStatic className="font-bold text-blue-400 mx-2">Madhya Pradesh, India</LinkPreview> passionate about delivering high-quality digital products. <span className="text-gray-500">My focus is on crafting intuitive, user-centered software that empowers users and drives meaningful change.</span>
       </p>
     </div>
--- components/Quote.tsx
+++ components/Quote.tsx
@@ -7,7 +7,7 @@ const Quote = () => {
       <div className="flex flex-col items-center w-full">
         <span className="text-5xl text-gray-400 mb-4"><PiQuotesFill /></span>
         <p className="text-neutral-400 dark:text-neutral-300 text-lg md:text-2xl max-w-4xl mx-auto text-left leading-relaxed mb-10">
-          Greatness lies in the extra 10%. It's about pushing limits, embracing details, and never settling for “good enough“. I believe the difference comes from grit, purpose, and the drive to go the extra mile. <span className="text-gray-500">Whether in code or design, my goal is to deliver work that stands out — because true impact comes from striving for excellence.</span>
+          Greatness lies in the extra 10%. It&apos;s about pushing limits, embracing details, and never settling for “good enough“. I believe the difference comes from grit, purpose, and the drive to go the extra mile. <span className="text-gray-500">Whether in code or design, my goal is to deliver work that stands out — because true impact comes from striving for excellence.</span>
         </p>
       </div>
     </div>
